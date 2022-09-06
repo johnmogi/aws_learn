c2e9179f6ca8fef68fb73607b064f95edf5a20d6
johnmogi <anguru@gmail.com>
2022-09-06 12:07:25 +0300

    up

--- aws_terraform.md
+++ aws_terraform.md
@@ -8,3 +8,10 @@ export AWS_SECRET_ACCESS_KEY=
 rm terraform.tfstate
 
 https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+
+https://cloud-images.ubuntu.com/locator/
+https://cidr.xyz/
+
+1. no igw - no internet gateway.
+2. no ip - no inbound rule for ssh 22
+3. no key pair. - converted aws.pem into public key
--- main.tf
+++ main.tf
@@ -15,22 +15,54 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_vpc" "weightapp_vpc" {
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = var.instance_name
+    Name = "tf-weightapp"
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.weightapp_vpc.id
+}
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+
+resource "aws_subnet" "weightapp_subnet" {
+  vpc_id            = aws_vpc.weightapp_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
-    Name = "main"
+    Name = "tf-weightapp"
   }
+}
+
+resource "aws_network_interface" "wa-interface" {
+  subnet_id   = aws_subnet.weightapp_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "wa_front" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+  key_name = "deployer-key"
+
+  network_interface {
+    network_interface_id = aws_network_interface.wa-interface.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD3YqYxXxeZy5966tARrDtOqkjxhkQuise5RJyXsTs67PB1TPsszf76FsIZLQQABywMPC//tNWi3EF+49Iw1c/AiplS28Iq3rlW/xRRrRqvZuhwOco3DW2R1usvae0EjugC98X+LmMEBrZvaX9RFas0F0oqX4U2pcLfBNjVwxn9d1TeaPU6KYVvX2rJrY9EKjmEpH0G9N3jide+3H5mdRdipfwwGb4n9hdefbJ89f2q7VksQIJEYtsnAXYL1yWqL05Ikl1n+APqwBe59ZOZp2zKyV1JYV7x7EfgNXdfI/F6tTqX0WWZJRB5OES/Wxla5Z8wXpAnyEKncE2Rv9WgFEJW4+qzhJBlx0HeNozwME185g8HZQh7gWzrQuUQzzYgIBKBtPAGQKTmAsksLwIQ5qDfbzvkp0ZcXd/E2vloySmTxxOT/kZgargkSU8orbRozfnp8px9Dd0HavrZ0UGb9/uf4WwEgkghPRzxEKlNR89boUzPkbhkrFsSeW/KH2OEqU= john@john-Z370P-D"
 }
